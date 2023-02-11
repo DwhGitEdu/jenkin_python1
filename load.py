88516144eaebf516fee7679eb2839231d4190ee4
entel_code_repo <eduardo.gonzales.pisco@entel.pe>
2023-02-11 09:50:06 -0500

    v3
    modified

--- load.py
+++ load.py
@@ -1,17 +1,20 @@
+#####################
+## proyecto pytho ##
+#####################
+
 import os
 
-List1 = [8,9,3,6,1,10]
+List1 = [8, 9, 3, 6, 1, 10]
 List1.reverse()
-print("The reversed list is" , List1)
+print("The reversed list is", List1)
 
-
-List2 = [81,92,33,64,11,101]
+List2 = [81, 92, 33, 64, 11, 101]
 List2.sort()
-print("The sorted list is" , List2)
+print("The sorted list is", List2)
 
 List3 = []
 List3 = List1.copy()
-print("The copy list" ,  List3)
+print("The copy list", List3)
 
 Indexvalue = List2[2:6]
-print("The index value " , Indexvalue)
+print("The index value ", Indexvalue)
