a7be512c35cd6ea31278193505f842793783f3e7
datahope_dwh_consulting@outlook.com <datahope_dwh_consulting@outlook.com>
2023-02-11 18:08:18 -0500

    v6 test

---
+++ test_load.py
@@ -0,0 +1,17 @@
+
+import os
+
+List1 = [8, 9, 3, 6, 1, 10]
+List1.reverse()
+print("The reversed list is", List1)
+
+List2 = [81, 92, 33, 64, 11, 101,504]
+List2.sort()
+print("The sorted list is", List2)
+
+List3 = []
+List3 = List1.copy()
+print("The copy list", List3)
+
+Indexvalue = List2[2:6]
+print("The index value ", Indexvalue)
